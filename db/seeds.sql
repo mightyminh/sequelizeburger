9f2919f3be17befe6b7b42a0071325edd1cb666b
mightyminh <minhtuyenmai@gmail.com>
2017-05-05 16:53:27 -0700

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Foot Feta-ish Burger
+', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('
+Mission A-Corn-Plished Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Olive And Let Die Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Chorizo Your Own Adventure Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Jingle Bell Pepper Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Not If I Can Kelp It Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('I Heartichoke You Burger', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Sweet Home Avocado Burger', false, CURRENT_TIMESTAMP);
